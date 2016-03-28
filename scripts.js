4b1bbcb9923914c231896a73d5efd854caa62192
Keith Moore <keith.m4@gmail.com>
2016-03-28 11:51:47 -0400

    Add or remove students from the class

--- scripts.js
+++ scripts.js
@@ -1,16 +1,49 @@
-/* THE VANILLA JS WAY
-var myInput = document.getElementById('my-input');
+//console.log(window.angular) shows that angular is an object in the root scope
+var myApp = angular.module('myApp', []);
+//console.log(myApp);
+//the controller method creates a controller that belongs to myApp
+//we pass controller an anonymous function
+//$scope is the object that glues the view and controller together
+//myController has access to everything inside $scope
+myApp.controller('myController', function($scope){
+	//$scope.first = "Keith";
+	//$scope.last = "Moore";
 
-myInput.addEventListener('keyup', function(){
-	//console.log('User hit a key.');
-	var userText = myInput.value;
-	document.getElementById('input-text').innerHTML = userText;
-});
-*/
+	/*
+	{
+		name: 'Tristan',
+		desc: 'Stock guy',
+		luckyNumber: 12
+	},
+	{
+		name: 'Josh',
+		desc: 'Designer extraordinaire',
+		luckyNumber: 3
+	}
+	*/
 
-/* THE JQUERY WAY
-$('#my-input').keyup(function() {
-	$('#input-text').html($('#my-input').val());
-});
-*/
+	$scope.dcClass = [
+		'Tristan',
+		'Josh',
+		'Bogdon',
+		'Lazlo',
+		'Keith',
+		'Will',
+		'Curtis',
+		'Joe',
+		'Kochan',
+		'Patrick',
+		'Jonathan',
+		'Jeremy'
+	];
 
+	$scope.addStudent = function(){
+		$scope.dcClass.push($scope.newStudent);
+	}
+
+	$scope.removeStudent = function(name){
+		console.log(name);
+		$scope.dcClass.splice($scope.dcClass[name], 1);
+	}
+
+})
