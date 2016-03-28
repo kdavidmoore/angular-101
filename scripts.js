d9d1783fdbf0b3777677753ab6d982d755cf260c
Keith Moore <keith.m4@gmail.com>
2016-03-28 12:59:54 -0400

    Add/remove as objects
    Each student is an object. Only removes students from the beginning of
    the array…

--- scripts.js
+++ scripts.js
@@ -9,41 +9,75 @@ myApp.controller('myController', function($scope){
 	//$scope.first = "Keith";
 	//$scope.last = "Moore";
 
-	/*
-	{
-		name: 'Tristan',
-		desc: 'Stock guy',
-		luckyNumber: 12
-	},
-	{
-		name: 'Josh',
-		desc: 'Designer extraordinaire',
-		luckyNumber: 3
-	}
-	*/
-
 	$scope.dcClass = [
-		'Tristan',
-		'Josh',
-		'Bogdon',
-		'Lazlo',
-		'Keith',
-		'Will',
-		'Curtis',
-		'Joe',
-		'Kochan',
-		'Patrick',
-		'Jonathan',
-		'Jeremy'
+		{
+			name: 'Tristan',
+			desc: 'Stock guy',
+			luckyNumber: 12
+		},
+		{
+			name: 'Josh',
+			desc: 'Designer extraordinaire',
+			luckyNumber: 3
+		},
+		{
+			name: 'Bogdon',
+			desc: 'Sik dude',
+			luckyNumber: 0
+		},
+		{
+			name: 'Lazlo',
+			desc: '?',
+			luckyNumber: 0
+		},
+		{
+			name: 'Keith',
+			desc: 'Rock man',
+			luckyNumber: 5730
+		},
+		{
+			name: 'Will',
+			desc: 'Ski guy',
+			luckyNumber: 32
+		},
+		{
+			name: 'Curtis',
+			desc: 'Java dev extraordinaire',
+			luckyNumber: 0
+		},
+		{
+			name: 'Joe',
+			desc: 'Code wizard',
+			luckyNumber: 10
+		},
+		{
+			name: 'Kochan',
+			desc: '?',
+			luckyNumber: 0
+		},
+		{
+			name: 'Patrick',
+			desc: '?',
+			luckyNumber: 0
+		},
+		{
+			name: 'Jonathan',
+			desc: 'The Handsome One',
+			luckyNumber: 0
+		},
+		{
+			name: 'Jeremy',
+			desc: '?',
+			luckyNumber: 0
+		}
 	];
 
-	$scope.addStudent = function(){
-		$scope.dcClass.push($scope.newStudent);
+	$scope.addStudent = function(name, desc, luckyNo){
+		$scope.dcClass.push({ name: $scope.newName, desc: $scope.newDesc, luckyNumber: $scope.newLuckyNo });
 	}
 
-	$scope.removeStudent = function(name){
-		console.log(name);
-		$scope.dcClass.splice($scope.dcClass[name], 1);
+	$scope.removeStudent = function(){
+		$scope.dcClass.splice(this, 1);
 	}
 
 })
